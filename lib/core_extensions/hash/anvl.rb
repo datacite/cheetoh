d214d84e10dab8dd84eb954a3f4586ad61df8d0b
Martin Fenner <martin.fenner@datacite.org>
2017-07-24 19:24:45 +0200

    escape % and : in anvl

--- lib/core_extensions/hash/anvl.rb
+++ lib/core_extensions/hash/anvl.rb
@@ -6,7 +6,7 @@ module CoreExtensions
   module Hash
     module Anvl
       def to_anvl
-        lines = self.map { |k, v| [k.to_s.anvlesc, v.to_s.anvlesc].join(": ") }
+        lines = self.map { |k, v| [k.to_s.kanvlesc, v.to_s.anvlesc].join(": ") }
         lines.join("\n").force_encoding(Encoding::UTF_8)
       end
     end
--- lib/core_extensions/string/anvl.rb
+++ lib/core_extensions/string/anvl.rb
@@ -4,17 +4,25 @@ module CoreExtensions
       def from_anvl
         self.split("\n").reduce({}) do |sum, line|
           k, v = line.split(": ", 2)
-          sum[k.to_s.anvlunesc] = v.to_s.anvlunesc
+          sum[k.to_s.kanvlunesc] = v.to_s.anvlunesc
           sum
         end
       end
 
+      def kanvlesc
+        self.anvlesc.gsub(/:/, "%3A")
+      end
+
       def anvlesc
-        self.gsub(/\n/, "%0A").gsub(/\r/, "%0D")
+        self.gsub(/%/, "%25").gsub(/\n/, "%0A").gsub(/\r/, "%0D")
+      end
+
+      def kanvlunesc
+        self.gsub(/%3A/, ":").anvlunesc
       end
 
       def anvlunesc
-        self.gsub(/%0A/, "\n").gsub(/%0D/, "\r")
+        self.gsub(/%25/, "%").gsub(/%0A/, "\n").gsub(/%0D/, "\r")
       end
     end
   end
