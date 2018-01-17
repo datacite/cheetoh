2128b189daa343f4ff04afad2e44984754b0219b
Martin Fenner <mfenner@datacite.org>
2018-01-17 11:12:15 +0100

    update model specs

--- spec/models/work_spec.rb
+++ spec/models/work_spec.rb
@@ -9,13 +9,13 @@ describe Work, vcr: true do
     it 'should convert' do
       lines = subject.hsh.to_anvl.split("\n")
       expect(lines[0]).to eq("success: doi:#{subject.doi}")
-      expect(lines[1]).to eq("_updated: #{subject._updated}")
-      expect(lines[2]).to eq("_target: #{subject.url}")
-      expect(lines[3].anvlunesc).to eq("datacite: #{subject.datacite}")
-      expect(lines[4]).to eq("_profile: datacite")
-      expect(lines[5]).to eq("_datacenter: #{subject.client_id}")
-      expect(lines[6]).to eq("_export: yes")
-      expect(lines[7]).to eq("_created: #{subject._created}")
+      expect(lines[1]).to eq("_target: #{subject.url}")
+      expect(lines[2].anvlunesc).to eq("datacite: #{subject.datacite}")
+      expect(lines[3]).to eq("_profile: datacite")
+      expect(lines[4]).to eq("_datacenter: #{subject.client_id}")
+      expect(lines[5]).to eq("_export: yes")
+      expect(lines[6]).to eq("_created: #{subject._created}")
+      expect(lines[7]).to eq("_updated: #{subject._updated}")
       expect(lines[8]).to eq("_status: public")
     end
   end
