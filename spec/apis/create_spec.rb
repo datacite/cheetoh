247aed702d763504c37238ac58193c374bc67a98
Martin Fenner <mfenner@datacite.org>
2018-01-31 09:38:31 +0000

    fixed specs

--- spec/apis/create_spec.rb
+++ spec/apis/create_spec.rb
@@ -102,8 +102,10 @@ describe "create", :type => :api, vcr: true, :order => :defined do
     expect(last_response.status).to eq(200)
     response = last_response.body.from_anvl
     expect(response["success"]).to eq("doi:10.5072/bc11-cqw7")
-    expect(response["datacite"]).to eq(datacite.strip)
     expect(response["_target"]).to eq(url)
     expect(response["_status"]).to eq("reserved")
+
+    doc = Nokogiri::XML(response["datacite"], nil, 'UTF-8', &:noblanks)
+    expect(doc.at_css("identifier").content).to eq("10.5072/BC11-CQW7")
   end
 end
